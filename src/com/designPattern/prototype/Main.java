c996e3564e98d636f70641f5517cc6f1b59b2776
Harish <harishharish3759@gmail.com>
2024-06-08 21:57:41 +0530

    Prototype design pattern

--- src/com/designPattern/prototype/Main.java
+++ src/com/designPattern/prototype/Main.java
@@ -1,5 +1,6 @@
 package com.designPattern.prototype;
 
+//The Prototype design pattern is a creational design pattern that enables the creation of new objects by copying an existing object, known as a prototype. This pattern is particularly useful when the cost of creating a new object is high and the creation process involves complex configurations or involves large data. In Java, the Prototype pattern typically involves implementing the Cloneable interface and overriding the clone() method.
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         BookShop bookShop = new BookShop();
