2a56d5253d106accc9a2a6a67d084eaf352695ff
Harish <harishharish3759@gmail.com>
2024-06-24 23:19:01 +0530

    Singleton design pattern

--- src/com/designPattern/singleton/Main.java
+++ src/com/designPattern/singleton/Main.java
@@ -1,5 +1,6 @@
 package com.designPattern.singleton;
 
+//The Singleton design pattern in Java is a creational design pattern that ensures a class has only one instance and provides a global point of access to that instance. This pattern is useful when exactly one object is needed to coordinate actions across the system.
 public class Main {
     public static void main(String[] args) {
         Singleton singleton = Singleton.getInstance();
